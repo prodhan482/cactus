28597adb2cdff287d68767f6be8e01fec9400fa0
Md. Dulal Prodhan <96787662+prodhan482@users.noreply.github.com>
2025-09-14 17:39:05 +0600

    initiated

---
+++ resources/views/web/pages/logobar.blade.php
@@ -0,0 +1,14 @@
+
+<section class="logo-bar py-5 my-5">
+    <div class="container">
+        <div class="row">
+            <div class="logo-content d-flex flex-wrap justify-content-between">
+                <img src="assets/images/logo1.png" alt="logo" class="logo-image img-fluid">
+                <img src="assets/images/logo2.png" alt="logo" class="logo-image img-fluid">
+                <img src="assets/images/logo3.png" alt="logo" class="logo-image img-fluid">
+                <img src="assets/images/logo4.png" alt="logo" class="logo-image img-fluid">
+                <img src="assets/images/logo5.png" alt="logo" class="logo-image img-fluid">
+            </div>
+        </div>
+    </div>
+</section>
